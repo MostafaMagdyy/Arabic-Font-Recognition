74e4ef06ee2df08c88731b4ec7d78802bbc8086d
MoMagdy_10 <manga9721@gmail.com>
2024-05-12 04:08:08 +0300

    fixing a bug

--- project/app.py
+++ project/app.py
@@ -78,5 +78,5 @@ def predict():
     
     return jsonify({'prediction': prediction})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
